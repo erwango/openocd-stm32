ca2a440df14284e9a38de0e70cafdd23c860a733
Erwan Gouriou <erwan.gouriou@linaro.org>
2017-02-03 09:53:47 +0100

    Add stm32 flash scripts
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

---
+++ setlocal.sh
@@ -0,0 +1,21 @@
+dir=`pwd`
+gdb_path=/local/mcu/gcc-arm-none-eabi-5_2-2015q4/bin/
+
+export PATH=$dir/src:$PATH
+export OPENOCD_SCRIPTS=$dir/tcl
+
+function gdb_connect() {
+$gdb_path/arm-none-eabi-gdb -ex "target remote localhost:3333" $1
+}
+
+function stm32_flsh {
+pkill openocd
+openocd -s $OPENOCD_SCRIPTS -f stm32"$1".cfg -c "stm_flash $2"
+}
+
+function stm32_dbg {
+pkill openocd
+openocd -s $OPENOCD_SCRIPTS -f  stm32"$1".cfg  &
+gdb_connect "$2"
+}
+
